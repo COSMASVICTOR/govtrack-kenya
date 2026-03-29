3bcf629373c514708b37a65835f4e3aa6649e791
COSMASVICTOR <optiolist@gmail.com>
2026-03-29 19:24:51 +0300

    Add detailed email debug logging

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,15 +1,19 @@
 const nodemailer = require('nodemailer');
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
-
 const sendEmail = async ({ to, subject, html }) => {
+  console.log('📧 Attempting to send email to:', to);
+  console.log('📧 EMAIL_USER:', process.env.EMAIL_USER ? 'SET' : 'NOT SET');
+  console.log('📧 EMAIL_PASS:', process.env.EMAIL_PASS ? 'SET' : 'NOT SET');
+
   try {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
     await transporter.sendMail({
       from: `"GovTrack Kenya" <${process.env.EMAIL_USER}>`,
       to,
